pulumi/terraform

commit e93d64b18c1ccc6e21609d47c6344141abe274d0

helper/schema: Opt-in panic on invalid ResourceData.Set

helper/schema/resource_data.go
@@ -35,6 +35,8 @@ type ResourceData struct {
 	partialMap  map[string]struct{}
 	once        sync.Once
 	isNew       bool
+
+	panicOnError bool
 }
 
 // getResult is the internal structure that is generated when a Get
@@ -184,7 +186,11 @@ func (d *ResourceData) Set(key string, value interface{}) error {
 		}
 	}
 
-	return d.setWriter.WriteField(strings.Split(key, "."), value)
+	err := d.setWriter.WriteField(strings.Split(key, "."), value)
+	if err != nil && d.panicOnError {
+		panic(err)
+	}
+	return err
 }
 
 // SetPartial adds the key to the final state output while

builtin/providers/test/resource.go
@@ -49,7 +49,6 @@ func testResource() *schema.Resource {
 			"computed_read_only": {
 				Type:     schema.TypeString,
 				Computed: true,
-				ForceNew: true,
 			},
 			"computed_from_required": {
 				Type:     schema.TypeString,

builtin/providers/test/resource_gh12183.go
@@ -47,7 +47,7 @@ func testResourceGH12183() *schema.Resource {
 
 func testResourceCreate_gh12183(d *schema.ResourceData, meta interface{}) error {
 	d.SetId("testId")
-	return testResourceRead(d, meta)
+	return testResourceRead_gh12183(d, meta)
 }
 
 func testResourceRead_gh12183(d *schema.ResourceData, meta interface{}) error {

helper/schema/resource_data_test.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"fmt"
 	"math"
+	"os"
 	"reflect"
 	"testing"
 	"time"
@@ -2054,6 +2055,10 @@ func TestResourceDataSet(t *testing.T) {
 			GetValue: "",
 		},
 	}
+
+	oldEnv := os.Getenv(PanicOnErr)
+	os.Setenv(PanicOnErr, "")
+	defer os.Setenv(PanicOnErr, oldEnv)
 
 	for i, tc := range cases {
 		d, err := schemaMap(tc.Schema).Data(tc.State, tc.Diff)

helper/schema/schema.go
@@ -24,6 +24,9 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+// Name of ENV variable which (if not empty) prefers panic over error
+const PanicOnErr = "TF_SCHEMA_PANIC_ON_ERROR"
+
 // type used for schema package context keys
 type contextKey string
 
@@ -357,6 +360,13 @@ func (s *Schema) finalizeDiff(
 // schemaMap is a wrapper that adds nice functions on top of schemas.
 type schemaMap map[string]*Schema
 
+func (m schemaMap) panicOnError() bool {
+	if os.Getenv(PanicOnErr) != "" {
+		return true
+	}
+	return false
+}
+
 // Data returns a ResourceData for the given schema, state, and diff.
 //
 // The diff is optional.
@@ -364,9 +374,10 @@ func (m schemaMap) Data(
 	s *terraform.InstanceState,
 	d *terraform.InstanceDiff) (*ResourceData, error) {
 	return &ResourceData{
-		schema: m,
-		state:  s,
-		diff:   d,
+		schema:       m,
+		state:        s,
+		diff:         d,
+		panicOnError: m.panicOnError(),
 	}, nil
 }
 
@@ -384,9 +395,10 @@ func (m schemaMap) Diff(
 	}
 
 	d := &ResourceData{
-		schema: m,
-		state:  s,
-		config: c,
+		schema:       m,
+		state:        s,
+		config:       c,
+		panicOnError: m.panicOnError(),
 	}
 
 	for k, schema := range m {

commit e37d9a70fa563f9e6ad01d63234b8c3fbf7654e3

Optionally panic on errors in ResourceState.State.
This extends the `panicOnError` behavior introduced in e93d64b1 to
errors that occur when writing the full attribute set for a ResourceData
value in ResourceState.State.

helper/schema/resource_data.go
@@ -315,6 +315,10 @@ func (d *ResourceData) State() *terraform.InstanceState {
 
 	mapW := &MapFieldWriter{Schema: d.schema}
 	if err := mapW.WriteField(nil, rawMap); err != nil {
+		if d.panicOnError {
+			panic(err)
+		}
+
 		return nil
 	}
 