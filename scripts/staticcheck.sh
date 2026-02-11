2f07806d1f720eb3e423655838622d55da6b726b
Zaid Ajaj <zaid.naom@gmail.com>
2026-02-11 18:30:01 +0100

    skip pkg/legacy when typechecking

--- scripts/staticcheck.sh
+++ scripts/staticcheck.sh
@@ -3,7 +3,7 @@
 echo "==> Checking that code complies with static analysis requirements..."
 # Skip legacy code which is frozen, and can be removed once we can refactor the
 # remote backends to no longer require it.
-skip="internal/legacy|backend/remote-state/"
+skip="pkg/legacy|internal/legacy|backend/remote-state/"
 
 # Skip generated code for protobufs.
 skip=$skip"|internal/planproto|internal/tfplugin5|internal/tfplugin6"
@@ -12,7 +12,7 @@ packages=$(go list ./... | egrep -v ${skip})
 
 # We are skipping style-related checks, since terraform intentionally breaks
 # some of these. The goal here is to find issues that reduce code clarity, or
-# may result in bugs. We also disable fucntion deprecation checks (SA1019)
+# may result in bugs. We also disable function deprecation checks (SA1019)
 # because our policy is to update deprecated calls locally while making other
 # nearby changes, rather than to make cross-cutting changes to update them all.
 go run honnef.co/go/tools/cmd/staticcheck -checks 'all,-SA1019,-ST*' ${packages}
